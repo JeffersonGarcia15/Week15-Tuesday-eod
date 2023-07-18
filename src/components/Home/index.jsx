bbc89f724562ff794bae23c1d30f2246fb23daa6
JeffersonGarcia15 <JeffersonGarcia1599@gmail.com>
2023-07-18 16:17:28 -0500

    Home component done

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllScientistsCreator } from "../../store/scientist";
+import { NavLink } from "react-router-dom";
 
 export function Home() {
+	const dispatch = useDispatch();
+	// const scientists = useSelector((state) => state.scientists); Do this to demo that we need to key into allScientists
+	const scientists = useSelector((state) => state.scientists.allScientists);
+    
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch("http://localhost:5000/scientists");
+			const scientists = await response.json();
+			dispatch(getAllScientistsCreator(scientists));
+		}
+		fetchData();
+	}, []);
+
 	return (
-		<h1>
-            Home
-		</h1>
+		<ul>
+			{Object.values(scientists).map((scientist) => (
+				<NavLink to={`/scientist/${scientist.id}`} key={scientist.id}>{scientist.name}</NavLink>
+			))}
+		</ul>
 	);
 }
 
--- src/store/scientist.js
+++ src/store/scientist.js
@@ -1,5 +1,23 @@
-export function scientistReducer(state = {}, action) {
+const GET_ALL_SCIENTISTS = "GET_ALL_SCIENTISTS";
+
+export function getAllScientistsCreator(scientists) {
+	return {
+		type: GET_ALL_SCIENTISTS,
+		scientists,
+	};
+}
+
+const initialState = { allScientists: {}, singleScientist: {} };
+export function scientistReducer(state = initialState, action) {
+	let newState;
 	switch (action.type) {
+	case GET_ALL_SCIENTISTS:
+		// newState = { ...state, allScientists: {}}; 
+		newState = structuredClone(state);
+		action.scientists.forEach((scientist) => {
+			newState.allScientists[scientist.id] = scientist;
+		});
+		return newState;
 	default:
 		return state;
 	}
